23c38dd6055f773dd94056f176269ccc4025ba3d
Gabriel Zurawski <gabrielzds03@gmail.com>
2026-06-01 23:15:46 -0300

    correção do numero de processos

--- ladEscalonamentoForte.sh
+++ ladEscalonamentoForte.sh
@@ -1,5 +1,5 @@
-srun -N 2 -n 2 ./mult_mpi 2048
-srun -N 2 -n 4 ./mult_mpi 2048
-srun -N 2 -n 8 ./mult_mpi 2048
-srun -N 2 -n 16 ./mult_mpi 2048
-srun -N 2 -n 32 ./mult_mpi 2048
+srun -N 2 -n 3 ./mult_mpi 2048
+srun -N 2 -n 5 ./mult_mpi 2048
+srun -N 2 -n 9 ./mult_mpi 2048
+srun -N 2 -n 17 ./mult_mpi 2048
+srun -N 3 -n 33 ./mult_mpi 2048
--- ladEscalonamentoFraco.sh
+++ ladEscalonamentoFraco.sh
@@ -1,5 +1,5 @@
-srun -N 2 -n 2 ./mult_mpi 2048
-srun -N 2 -n 4 ./mult_mpi 2555
-srun -N 2 -n 8 ./mult_mpi 3317
-srun -N 2 -n 16 ./mult_mpi 4226
-srun -N 2 -n 32 ./mult_mpi 5286
+srun -N 2 -n 3 ./mult_mpi 2048
+srun -N 2 -n 5 ./mult_mpi 2555
+srun -N 2 -n 9 ./mult_mpi 3317
+srun -N 2 -n 17 ./mult_mpi 4226
+srun -N 3 -n 33 ./mult_mpi 5286
